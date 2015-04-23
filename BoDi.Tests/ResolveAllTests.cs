554bb4fd15737c9b66aed3139982205f5a6bd86c
Darren Cauthon <darren@cauthon.com>
2015-04-23 06:48:18 -0500

    Spacing.

--- BoDi.Tests/ResolveAllTests.cs
+++ BoDi.Tests/ResolveAllTests.cs
@@ -7,47 +7,47 @@ using NUnit.Framework;
 namespace BoDi.Tests
 {
 
-	public interface IFancy{}
-	public class ImFancy : IFancy {}
-	public class ImFancier : IFancy {}
-	public class ImFanciest : IFancy {}
+    public interface IFancy{}
+    public class ImFancy : IFancy {}
+    public class ImFancier : IFancy {}
+    public class ImFanciest : IFancy {}
 
+    [TestFixture]
+    public class ResolveAllTests
+    {
 
-	[TestFixture]
-	public class ResolveAllTests
-	{
+        [Test]
+        public void ShouldResolveTheRightNumberOfRegisteredTypes()
+        {
+            // given
+            var container = new ObjectContainer();
+            container.RegisterTypeAs<ImFancy, IFancy>("fancy");
+            container.RegisterTypeAs<ImFancier, IFancy>("fancier");
+            container.RegisterTypeAs<ImFanciest, IFancy>("fanciest");
 
-		[Test]
-		public void ShouldResolveTheRightNumberOfRegisteredTypes()
-		{
-			// given
-			var container = new ObjectContainer();
-			container.RegisterTypeAs<ImFancy, IFancy>("fancy");
-			container.RegisterTypeAs<ImFancier, IFancy>("fancier");
-			container.RegisterTypeAs<ImFanciest, IFancy>("fanciest");
+            // when
+            var results = container.ResolveAll<IFancy>();
 
-			// when
-			var results = container.ResolveAll<IFancy>();
+            // then
+            Assert.AreEqual(3, results.Count());
+        }
 
-			// then
-			Assert.AreEqual(3, results.Count());
-		}
+        [Test]
+        public void ShouldResolveTheRightTypes()
+        {
+            // given
+            var container = new ObjectContainer();
+            container.RegisterTypeAs<ImFancy, IFancy>("fancy");
+            container.RegisterTypeAs<ImFancier, IFancy>("fancier");
 
-		[Test]
-		public void ShouldResolveTheRightTypes()
-		{
-			// given
-			var container = new ObjectContainer();
-			container.RegisterTypeAs<ImFancy, IFancy>("fancy");
-			container.RegisterTypeAs<ImFancier, IFancy>("fancier");
+            // when
+            var results = container.ResolveAll<IFancy>();
 
-			// when
-			var results = container.ResolveAll<IFancy>();
+            // then
+            Assert.IsTrue(results.Contains(container.Resolve<IFancy>("fancy")));
+            Assert.IsTrue(results.Contains(container.Resolve<IFancy>("fancier")));
+        }
 
-			// then
-			Assert.IsTrue(results.Contains(container.Resolve<IFancy>("fancy")));
-			Assert.IsTrue(results.Contains(container.Resolve<IFancy>("fancier")));
-		}
+    }
 
-	}
 }
